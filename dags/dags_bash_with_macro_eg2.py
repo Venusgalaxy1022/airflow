79d77acbff270b254576016195781d878dd1e1db
Venusgalaxy <Venusgalaxy1022@gmail.com>
2025-12-25 10:39:20 +0900

    create dags_bash_with_macros

--- dags/dags_bash_with_macro_eg2.py
+++ dags/dags_bash_with_macro_eg2.py
@@ -12,8 +12,8 @@ with DAG(
 # start_date: 2주전 월요일, end_date: 2주전 토요일
     bash_t1 = BashOperator(
         task_id="bash_t1",
-        env = {'START_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul" - macros.dateutil.relativedelta.relativedelta(days=-19)) | ds)}}', # yyyy-mm-dd
-                'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul" - macros.dateutil.relativedelta.relativedelta(days=-14)) | ds)}}'    
+        env = {'START_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=-19)) | ds)}}', # yyyy-mm-dd
+                'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=-14)) | ds)}}'    
         },
         bash_command= 'echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
     )
