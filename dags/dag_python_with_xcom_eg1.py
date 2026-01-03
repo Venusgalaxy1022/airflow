8f67cc397b33795519d4e021dc8a64f9347e64d5
Venusgalaxy <Venusgalaxy1022@gmail.com>
2026-01-03 12:01:46 +0900

    revise a dag with xcom (add pull task)

--- dags/dag_python_with_xcom_eg1.py
+++ dags/dag_python_with_xcom_eg1.py
@@ -9,7 +9,6 @@ with DAG(
     start_date = pendulum.datetime(2026, 1, 3, tz='Asia/Seoul'),
     catchup = False,
 ) as dag: 
-
     @task(task_id='python_xcom_push_task1')
     def xcom_push1(**kwargs):
         ti = kwargs['ti']
@@ -21,6 +20,14 @@ with DAG(
         ti = kwargs['ti']
         ti.xcom_push(key="result1", value="value_2")
         ti.xcom_push(key="result2", value=[1,2,3,4])
+
+    @task(task_id='python_xcom_pull_task')
+    def xcom_pull(**kwargs):
+        ti = kwargs['ti']
+        value1 = ti.xcom_pull(key="result1")
+        value2 = ti.xcom_pull(key="result2", task_ids='python_xcom_push_task1')
+        print(value1)
+        print(value2)
 
 xcom_push1() >> xcom_push2() >> xcom_pull()
 
