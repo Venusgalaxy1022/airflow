473c5fd0b1e9ee73af75202ef43b89a58167d565
Venusgalaxy <Venusgalaxy1022@gmail.com>
2026-01-03 11:52:04 +0900

    create dags with xcoms

---
+++ dags/dag_python_with_xcom_eg1.py
@@ -0,0 +1,26 @@
+from airflow import DAG 
+import pendulum 
+import datetime 
+from airflow.decorators import task 
+
+with DAG(
+    dag_id = "dag_python_with_xcom_eg1",
+    schedule = "30 6 * * *", 
+    start_date = pendulum.datetime(2026, 1, 3, tz='Asia/Seoul'),
+    catchup = False,
+) as dag: 
+
+    @task(task_id='python_xcom_push_task1')
+    def xcom_push1(**kwargs):
+        ti = kwargs['ti']
+        ti.xcom_push(key="result1", value="value_1")
+        ti.xcom_push(key="result2", value=[1,2,3])
+    
+    @task(task_id='python_xcom_push_task2')
+    def xcom_push2(**kwargs):
+        ti = kwargs['ti']
+        ti.xcom_push(key="result1", value="value_2")
+        ti.xcom_push(key="result2", value=[1,2,3,4])
+
+xcom_push1() >> xcom_push2() >> xcom_pull()
+
